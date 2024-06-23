3ad7930c13d576239412185172a2433a1d7fc88e
takiguchi-yu <takiguchi-yu@gnavi.co.jp>
2024-06-23 18:08:42 +0900

    Authリゾルバーの実装

--- api/src/auth/auth.resolver.ts
+++ api/src/auth/auth.resolver.ts
@@ -1,4 +1,20 @@
-import { Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
+import { AuthService } from 'src/auth/auth.service';
+import { SignInInput } from 'src/auth/dto/signIn.input';
+import { SignInResponse } from 'src/auth/dto/signInResponse';
+import { GqlAuthGuard } from 'src/auth/guards/gql-auth.guard';
 
 @Resolver()
-export class AuthResolver {}
+export class AuthResolver {
+  constructor(private readonly authService: AuthService) {}
+
+  @Mutation(() => SignInResponse)
+  @UseGuards(GqlAuthGuard)
+  async signIn(
+    @Args('signInInput') signInInput: SignInInput,
+    @Context() context: any,
+  ) {
+    return await this.authService.signIn(context.user);
+  }
+}
