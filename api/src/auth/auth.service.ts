bbde10890af3d2cbb4cc0eabfa393de482461047
takiguchi-yu <takiguchi-yu@gnavi.co.jp>
2024-06-23 17:46:17 +0900

    feat: :sparkles: Authサービスの実装

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+import { SignInResponse } from 'src/auth/dto/signInResponse';
+import { JwtPayload } from 'src/auth/types/jwtPayload.type';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async validateUser(email: string, password: string): Promise<User | null> {
+    const user = await this.userService.getUser(email);
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return user;
+    }
+    return null;
+  }
+
+  async signIn(user: User): Promise<SignInResponse> {
+    const payload: JwtPayload = { email: user.email, sub: user.id };
+    return { accessToken: this.jwtService.sign(payload), user };
+  }
+}
