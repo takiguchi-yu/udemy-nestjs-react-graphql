96c4a19127c168e0684f0db47d690d78aa801bdf
takiguchi-yu <takiguchi-yu@gnavi.co.jp>
2024-06-30 13:49:31 +0900

    認証ルーティング実装

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,14 +1,33 @@
-import SignIn from "@/components/SignIn";
-import SignUp from "@/components/SignUp";
 import Main from "@/components/Main";
+import { Payload } from "@/types/payload";
+import { jwtDecode } from "jwt-decode";
+import { cookies } from "next/headers";
+import { redirect } from 'next/navigation';
 import styles from "./page.module.css";
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <SignIn />
-      <SignUp />
-      <Main />
-    </main>
-  );
+export default async function HomePage() {
+  const token = cookies().get("token")?.value;
+
+  if (token) {
+    try {
+      const decodedToken = jwtDecode<Payload>(token);
+      // トークンの有効期限切れ
+      if (decodedToken.exp * 1000 < Date.now()) {
+        redirect("/signin");
+      } else {
+        // 認証済み
+        return (
+          <main className={styles.main}>
+            <Main />
+          </main>
+        );
+      }
+    } catch (error) {
+      // デコードエラー
+      redirect("/signin");
+    }
+  } else {
+    // トークンがない場合
+    redirect("/signin");
+  }
 }
